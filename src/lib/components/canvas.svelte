bd753536b10c9876a15f57b28828685918cad15f
bitsmyu <bitsmyu@gmail.com>
2025-02-01 14:49:39 +0900

    added load animation

--- src/lib/components/canvas.svelte
+++ src/lib/components/canvas.svelte
@@ -16,13 +16,10 @@
 <!-- ローディングアニメ―ション -->
 <div id="load" class:loaded class:loadComp>
   <div class="load-main">
-    <div class="tr tr-1"></div>
-    <div class="tr tr-2"></div>
-    <div class="tr tr-3"></div>
-    <div class="tr tr-4"></div>
-    <div class="tr tr-5"></div>
+      <img class="tr tr-1" src="./fl-1.png">
+      <img class="tr tr-2" src="./fl-2.png">
+      <img class="tr tr-3" src="./fl-3.png">
   </div>
-  <div class="load-back"></div>
 </div>
 
 <style>
@@ -31,47 +28,22 @@
     position: fixed;
     height: 100vh;
     width: 100vw;
-    background-color: #e26400;
+    background-color: #0081CC;
     z-index: 9999;
     transition: all 0.5s;
   }
   #load.loaded .load-main {
     animation: loaded 3s;
   }
-  #load.loadComp .load-main,
-  #load.loadComp .load-back {
+  #load.loadComp .load-main
+  {
     opacity: 0;
     visibility: hidden;
     z-index: -999;
   }
-  .load-back {
-    position: fixed;
-    height: 100vh;
-    width: 100vw;
-    background-color: #5fc499;
-    z-index: 8888;
-  }
-  #load.loaded .load-back {
-    animation: loadedback 3s;
-  }
   @keyframes loaded {
     10% {
-      transform: translateX(-7%);
-    }
-    75% {
       opacity: 1;
-    }
-    100% {
-      transform: translateX(110%);
-      opacity: 0;
-    }
-  }
-  @keyframes loadedback {
-    15% {
-      opacity: 1;
-    }
-    20% {
-      opacity: 0;
     }
     100% {
       opacity: 0;
@@ -80,224 +52,53 @@
 
   /* ローディング折り紙アニメーション */
   .tr {
-    background-color: #ccd23a;
     position: fixed;
-    height: 80px;
-    width: 80px;
   }
   .tr-1 {
-    left: calc(50vw + 1px);
-    top: calc(48vh + 1px);
-    transform-origin: 0% 0%;
-    animation: ani1 4s infinite linear;
-    clip-path: polygon(0 0, 0% 100%, 100% 0%);
+    left: 50%;
+    top: 50%;
+    transform: translateX(-50%) translateY(-50%);
+    width: 10%;
+    min-width: 100px;
+    height: auto;
+    z-index: 1111;
+    animation: ani1 4s infinite ease-in-out;
   }
   .tr-2 {
-    left: calc(50vw - 81px);
-    top: calc(48vh + 1px);
-    transform-origin: 100% 0;
-    animation: ani0 4s infinite linear;
-    clip-path: polygon(0 0, 100% 0%, 100% 100%);
+    left: 50%;
+    top: 50%;
+    transform: translateX(-50%) translateY(-50%);
+    width: 15%;
+    min-width: 150px;
+    height: auto;
+    z-index: 1110;
+    animation: ani2 5s infinite ease-in-out;
   }
   .tr-3 {
-    left: calc(50vw - 81px);
-    top: calc(48vh - 81px);
-    transform-origin: 100% 0;
-    animation: ani2 4s infinite linear;
-    clip-path: polygon(100% 0%, 0% 100%, 100% 100%);
-  }
-  .tr-4 {
-    left: calc(50vw + 1px);
-    top: calc(48vh - 81px);
-    transform-origin: 0 0;
-    animation: ani3 4s infinite linear;
-    z-index: 99999;
-    clip-path: polygon(0 0, 0% 100%, 100% 100%);
-  }
-  .tr-5 {
-    left: calc(50vw + 1px);
-    top: calc(48vh - 81px);
-    background-color: #5fc499;
-    transform-origin: 0 0;
-    animation: ani4 4s infinite linear;
-    clip-path: polygon(0 0, 0% 100%, 100% 100%);
-  }
-  @keyframes ani0 {
-    0% {
-      transform: scaleX(100%);
-      background-color: #ccd23a;
-    }
-    9% {
-      transform: scaleX(0%) scaleY(100%);
-      background-color: #9a9e2b;
-    }
-    18% {
-      transform: scaleX(0%) scaleY(0%);
-    }
-    36% {
-      transform: scaleX(100%) scaleY(0%);
-      background-color: #469170;
-    }
-    45% {
-      transform: scaleX(100%) scaleY(100%);
-      background-color: #5fc499;
-    }
-    54% {
-      transform: scaleX(0%) scaleY(100%);
-      background-color: #469170;
-    }
-    63% {
-      transform: scaleX(0%) scaleY(0%);
-    }
-    81% {
-      transform: scaleX(100%) scaleY(0%);
-      background-color: #9a9e2b;
-    }
-    90% {
-      transform: scaleY(100%);
-      background-color: #ccd23a;
-    }
+    left: 50%;
+    top: 50%;
+    transform: translateX(-50%) translateY(-50%);
+    width: 20%;
+    min-width: 200px;
+    height: auto;
+    z-index: 1109;
+    animation: ani1 6s infinite ease-in-out;
   }
   @keyframes ani1 {
     9% {
-      transform: scaleY(100%);
-      background-color: #ccd23a;
+      transform: translateX(-50%) translateY(-50%) rotateZ(0deg);
     }
-    18% {
-      transform: scaleY(0%);
-      background-color: #9a9e2b;
-    }
-    36% {
-      transform: scaleY(0%);
-      background-color: #9a9e2b;
-    }
-    37% {
-      background-color: #469170;
-    }
-    45% {
-      transform: scaleY(100%);
-      background-color: #5fc499;
-    }
-    54% {
-      transform: scaleY(100%);
-      background-color: #5fc499;
-    }
-    63% {
-      transform: scaleY(0%);
-      background-color: #469170;
-    }
-    81% {
-      transform: scaleY(0%);
-      background-color: #9a9e2b;
-    }
-    90% {
-      transform: scaleY(100%);
-      background-color: #ccd23a;
+    100% {
+      transform: translateX(-50%) translateY(-50%) rotateZ(360deg);
     }
   }
   @keyframes ani2 {
-    0% {
-      transform: scaleX(100%);
-      background-color: #ccd23a;
-    }
     9% {
-      transform: scaleX(0%);
-      background-color: #9a9e2b;
+      transform: translateX(-50%) translateY(-50%) rotateZ(0deg);
     }
-    27% {
-      transform: scaleX(0%);
-      background-color: #469170;
-    }
-    36% {
-      transform: scaleX(100%);
-      background-color: #5fc499;
-    }
-    45% {
-      transform: scaleX(100%) scaleY(100%);
-      background-color: #5fc499;
-    }
-    54% {
-      transform: scaleX(0%) scaleY(100%);
-      background-color: #469170;
-    }
-    72% {
-      transform: scaleX(0%) scaleY(100%);
-      background-color: #9a9e2b;
-    }
-    81% {
-      transform: scaleX(100%);
-      background-color: #ccd23a;
+    100% {
+      transform: translateX(-50%) translateY(-50%) rotateZ(-360deg);
     }
   }
-  @keyframes ani3 {
-    18% {
-      transform: scaleX(100%);
-      background-color: #ccd23a;
-    }
-    27% {
-      transform: scaleX(0%);
-      background-color: #9a9e2b;
-    }
-    36% {
-      transform: scaleX(0%) scaleY(100%);
-      background-color: #9a9e2b;
-    }
-    45% {
-      transform: scaleX(0%) scaleY(0%);
-    }
-    54% {
-      transform: scaleX(0%) scaleY(0%);
-      z-index: 9999;
-    }
-    62% {
-      transform: scaleX(100%) scaleY(100%);
-      background-color: #9a9e2b;
-    }
-    63% {
-      transform: scaleX(100%) scaleY(100%);
-      background-color: #9a9e2b;
-    }
-    72% {
-      background-color: #ccd23a;
-    }
-  }
-  @keyframes ani4 {
-    18% {
-      transform: scaleX(100%) scaleY(100%);
-
-      background-color: #469170;
-    }
-    27% {
-      background-color: #5fc499;
-    }
-    36% {
-      background-color: #5fc499;
-      z-index: 9999;
-    }
-    45% {
-      z-index: 99999;
-      background-color: #5fc499;
-    }
-    62% {
-      transform: scaleX(100%) scaleY(100%);
-      background-color: #5fc499;
-    }
-    63% {
-      transform: scaleX(100%) scaleY(100%);
-      background-color: #5fc499;
-    }
-    72% {
-      transform: scaleX(0%) scaleY(100%);
-      z-index: 99999;
-      background-color: #469170;
-    }
-    90% {
-      z-index: 9999;
-      transform: scaleX(0%) scaleY(100%);
-    }
-    99% {
-      transform: scaleX(100%) scaleY(100%);
-    }
-  }
-  /* 折り紙アニメーションここまで */
+  /* アニメーションここまで */
 </style>
