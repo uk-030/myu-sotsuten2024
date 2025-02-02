5d7764e913245ff90498b1bf1dc1089dc4fa728e
Arai-Ch14 <kaikiarai517@gmail.com>
2025-02-02 21:58:53 +0900

    Update key-visual.svelte
    文字情報可変対応用

--- src/lib/components/key-visual.svelte
+++ src/lib/components/key-visual.svelte
@@ -1,17 +1,22 @@
 <section class="relative h-svh">
   <img
-    src="./top-pc.png"
+    src="./top-pc 1.png"
     alt="背景画像"
     class="hidden h-full min-w-full object-cover md:block"
   />
   <img
-    src="./top-sp.png"
+    src="./top-sp 1.png"
     alt="背景画像"
     class="h-full w-full object-cover md:hidden"
   />
   <img
-	  src="./title-v2.png"
-	  alt="タイトル画像"
-	  class="bg-secondary/50 title-img absolute right-6 top-0 md:max-w-[120vw] md:block sm:hidden"
+　　src="./title-v2.png"
+　　alt="タイトル画像"
+　　class="bg-secondary/50 title-img absolute right-6 top-0 md:max-w-[120vw] md:block sm:hidden"
+  />
+  <img
+　　src="./title-v2-1.png"
+　　alt="タイトル画像"
+　　class="bg-secondary/50 title-img absolute right-6 top-0 p-7 md:max-w-[40vw] md:hidden "
   />
 </section>
