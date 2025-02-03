8af74b39d702f8b53d4e72460117eef4ca9d2b7e
Arai-Ch14 <kaikiarai517@gmail.com>
2025-02-03 23:10:59 +0900

    Update key-visual.svelte

--- src/lib/components/key-visual.svelte
+++ src/lib/components/key-visual.svelte
@@ -17,6 +17,6 @@
   <img
 　　src="./title-v2-1.png"
 　　alt="タイトル画像"
-　　class="bg-secondary/50 title-img absolute right-6 top-0 p-8 py-7 md:max-w-[40vw] md:hidden "
+　　class="bg-secondary/50 title-img absolute right-6 top-0 p-8 py-3 md:max-w-[40vw] md:hidden "
   />
 </section>
